ed89a028d19a28d69f2cfc6eddbec65de92d6a0b
Harikrushna16 <patelharikrushan8416@gmail.com>
2025-09-10 21:30:29 +0530

    added domains for next.config file

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    domains: ["img.daisyui.com", "image.tmdb.org"],
+  },
+  reactStrictMode: true,
 };
 
 export default nextConfig;
